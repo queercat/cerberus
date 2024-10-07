d2d5746779ab03d696eae565d149d355854a25fe
May Tusek <128516535+may-20i@users.noreply.github.com>
2024-10-06 21:38:14 -0700

    Adds script for and begins adding frontend.

---
+++ frontend/src/hooks/useGetFiles.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react"
+
+export const useGetFiles = () => {
+    const [files, setFiles] = useState<{title: string, uuid: string}[]>([])
+
+    const getFiles = async () => {
+        const response = await fetch('http://localhost:4000/files')
+
+        const data = await response.json()
+
+        setFiles(data)
+    }
+
+    useEffect(() => {
+        getFiles()
+    }, [])
+
+    return {files, getFiles}
+}
